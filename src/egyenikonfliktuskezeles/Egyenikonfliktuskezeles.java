0cc63d26c50ef33adc0b1fde86d25c12fb701fb6
PIKSSL <purpledog120@gmail.com>
2022-03-07 12:08:50 +0100

    1.név deklarálása

---
+++ src/egyenikonfliktuskezeles/Egyenikonfliktuskezeles.java
@@ -0,0 +1,12 @@
+package egyenikonfliktuskezeles;
+public class Egyenikonfliktuskezeles {
+
+    public static void main(String[] args) {
+        String nev1;
+        
+        
+        
+        
+    }
+    
+}
