80360d145faddf5244c6be7ecf78637bf9e71169
PIKSSL <95695591+PIKSSL@users.noreply.github.com>
2022-03-07 12:24:40 +0100

    3. név deklarálása

--- src/egyenikonfliktuskezeles/Egyenikonfliktuskezeles.java
+++ src/egyenikonfliktuskezeles/Egyenikonfliktuskezeles.java
@@ -2,10 +2,10 @@ package egyenikonfliktuskezeles;
 public class Egyenikonfliktuskezeles {
 
     public static void main(String[] args) {
-
+        int i = 7;
         String nev1 = "GyK";
         String nev2 = "X.Y.";
-
+        int nev3;
         
 
         
